c14ba2fb76d7d75bc73469f69c37e0eac63bf008
chahdzhaira <chahd.zhaira@esprit.tn>
2024-02-15 12:16:26 +0100

    atelier3

---
+++ src/components/Event.jsx
@@ -0,0 +1,32 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import React, { useState } from 'react';
+export default function Event (props){
+  
+    const [e , setEvent ]=useState(props.e)
+    const bookEvent = ()=> {props.fonctionAlerte()
+        setEvent(
+            (previousE) => ({...previousE ,"nbParticipants":previousE.nbParticipants+1 , "nbTickets":previousE.nbTickets-1})
+            )
+        }
+    const changeLike =()=>{
+        setEvent ((previousE) => ({ ...previousE , like : !previousE.like}))
+    }
+    return <>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={e.nbTickets ===0 ? "images/sold_out.png" : `images/${e.img}`}/>
+      <Card.Body>
+        <Card.Title>{e.name}</Card.Title>
+        <Card.Text>
+            {e.description}
+        </Card.Text>
+        <p>price : {e.price}</p>
+        <p>nbTickets: {e.nbTickets}</p>
+        <p>nbParticipants: {e.nbParticipants}</p>
+        <p></p>
+        <Button variant="danger" onClick={changeLike} >{e.like ? "Dislike" : "Like" }</Button>
+        <Button variant="primary" onClick={bookEvent} disabled={e.nbTickets=== 0 ? true : false }>Book an event </Button>
+      </Card.Body>
+    </Card>
+    </>
+}
---
+++ src/components/Events.jsx
@@ -0,0 +1,40 @@
+import Event from "./Event"
+import listEvent from "../data/events.json"
+import {Row} from "react-bootstrap"
+import { useEffect, useState } from "react"
+import Alert from 'react-bootstrap/Alert';
+export default function Events (){
+    const[ showAlert , setShowAlert ] = useState(false)
+    const[ showWelcome , setShowWelcome ] = useState(false)
+    const modifAlert= () => {
+        setShowAlert(true) 
+        setTimeout(()=> setShowAlert(false), 1000)
+    }
+    useEffect (()=> {
+        setShowWelcome(true)
+        setTimeout(()=> setShowWelcome(false), 3000) 
+        return ()=> { 
+            console.log("Welcome unmouting")
+            clearTimeout(() => setShowWelcome)
+        }
+    }, []) 
+
+    return <>
+    {
+        showWelcome && <Alert variant="sucess">
+        Bienvenu ! 
+      </Alert>
+    } 
+    {/* Pour eviter le probleme wa9t tebda aana data fergha maa el .map twali naamlou ?.map  */}
+    <Row>
+    {
+        listEvent?.map((element , index)=> {
+            return  <Event key={index} e={element} fonctionAlerte={modifAlert} />
+        })
+    }
+    </Row>
+    {showAlert && <Alert variant="sucess">
+          You have booked an event ! 
+        </Alert>}
+    </>
+}
